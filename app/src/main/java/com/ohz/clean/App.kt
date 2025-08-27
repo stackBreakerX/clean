6c8a176a3171a97464396e87e71e2003d8c6b4b3
altonzzw <altonzzw@gmail.com>
2025-08-28 00:13:29 +0800

    1.add permission check module

--- app/src/main/java/com/ohz/clean/App.kt
+++ app/src/main/java/com/ohz/clean/App.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.ohz.clean.coil.CoilTempFiles
 import com.ohz.clean.common.memory.MemoryMonitor
 import dagger.hilt.android.HiltAndroidApp
+import eu.darken.sdmse.common.coroutine.AppScope
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 /**
@@ -13,6 +15,8 @@ import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
+
+    @Inject @AppScope lateinit var appScope: CoroutineScope
 
 //    @Inject lateinit var coilTempFiles: CoilTempFiles
 //    @Inject lateinit var memoryMonitor: MemoryMonitor
--- app/src/main/java/com/ohz/clean/ui/fragment/MainViewModel.kt
+++ app/src/main/java/com/ohz/clean/ui/fragment/MainViewModel.kt
@@ -9,12 +9,19 @@ import com.ohz.clean.ui.base.ViewModel3
 import com.ohz.clean.ui.view.progress.Progress
 import dagger.hilt.android.lifecycle.HiltViewModel
 import eu.darken.sdmse.common.coroutine.DispatcherProvider
+import eu.darken.sdmse.common.flow.intervalFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
+import java.time.Duration
+import java.time.Instant
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * @description
@@ -43,6 +50,25 @@ class MainViewModel @Inject constructor(
     ) { data, progress ->
         data
     }.asLiveData2()
+
+    private val setupCardItem: Flow<SetupManager.State?> = setupManager.state
+        .flatMapLatest { setupState ->
+            if (setupState.isDone || setupState.isDismissed) return@flatMapLatest flowOf(null)
+
+            if (setupState.isIncomplete) return@flatMapLatest flowOf(setupState)
+
+            if (!setupState.isLoading) return@flatMapLatest flowOf(null)
+
+            intervalFlow(1.seconds).map {
+                val now = Instant.now()
+                val loadingStart = setupState.startedLoadingAt ?: now
+                if (Duration.between(loadingStart, now) >= Duration.ofSeconds(5)) {
+                    setupState
+                } else {
+                    null
+                }
+            }
+        }
 
 
 
